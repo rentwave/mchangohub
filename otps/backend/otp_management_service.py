3f4c2bed07522d642233a9b3ae27d7966e241b1a
SteveNdegwa <stevencallistus19@gmail.com>
2025-08-11 10:27:17 +0300

    refactor: notifications management service

--- otps/backend/otp_management_service.py
+++ otps/backend/otp_management_service.py
@@ -90,11 +90,11 @@ class OTPManagementService:
             if not token:
                 raise ValueError("Token must be provided for 2FA purpose")
 
+            # TODO: REVIEW WHETHER WE SHOULD CHECK FOR EXPIRES AT HERE - IS IT NECESSARY?
             identity = IdentityService().filter(
                 token=token,
                 status=Identity.Status.ACTIVATION_PENDING,
-                expires_at__gte=timezone.now(),
-            ).first()
+            ).order_by("-date_created").first()
 
             if identity is None:
                 raise Exception("Identity not found")
@@ -181,7 +181,11 @@ class OTPManagementService:
             if not token:
                 raise ValueError("Token must be provided for 2FA purpose")
 
-            identity = IdentityService().get(token=token, expires_at__gte=timezone.now())
+            # TODO: REVIEW WHETHER WE SHOULD CHECK FOR EXPIRES AT HERE - IS IT NECESSARY?
+            identity = IdentityService().get(
+                token=token,
+                status=Identity.Status.ACTIVATION_PENDING
+            )
             if identity is None:
                 raise Exception("Identity not found or expired")
 
@@ -214,12 +218,11 @@ class OTPManagementService:
             otp.save()
             raise ValueError("Incorrect OTP.")
 
-        otp.is_used = True
-
         if otp.identity:
             otp.identity.status = Identity.Status.ACTIVE
-            otp.identity.save()
+            otp.identity.extend()
 
+        otp.is_used = True
         otp.save()
 
         return otp
