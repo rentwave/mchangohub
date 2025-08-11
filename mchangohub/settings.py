a904a2e10a41ef5443b5cb14c1664e087862e32a
Echenje <stephen.osiru@spinmobile.co.ke>
2025-08-11 16:55:45 +0300

    update on deploy

--- mchangohub/settings.py
+++ mchangohub/settings.py
@@ -86,23 +86,23 @@ AUTH_USER_MODEL = "users.User"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': os.environ.get('POSTGRES_DB'),
-		'USER': os.environ.get('POSTGRES_USER'),
-		'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-		'HOST': os.environ.get('POSTGRES_HOST'),
-		'PORT': os.environ.get('POSTGRES_PORT'),
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.postgresql',
+# 		'NAME': os.environ.get('POSTGRES_DB'),
+# 		'USER': os.environ.get('POSTGRES_USER'),
+# 		'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+# 		'HOST': os.environ.get('POSTGRES_HOST'),
+# 		'PORT': os.environ.get('POSTGRES_PORT'),
+# 	}
+# }
 
 
 
