972b17fa7b8ea6240182ff6a0e8ff816ad435b16
Osiru <marikwamari023@gmail.com>
2025-08-15 13:17:28 +0400

    update master

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -e
 
 python manage.py collectstatic --no-input
-PORT=8055
+
+PORT=${PORT:-8055}
+
 exec gunicorn \
-    --bind 0.0.0.0:$PORT \
-    --access-logfile - \
+    --bind "0.0.0.0:${PORT}" \
+    --access-logfile "-" \
     --timeout 3600 \
-    mchangohub.wsgi:application -w 2
+    --workers 2 \
+    mchangohub.wsgi:application
